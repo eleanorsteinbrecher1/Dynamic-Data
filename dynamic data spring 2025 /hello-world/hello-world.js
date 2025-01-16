d5f1b70da10d178e48cd8dfe9b3a1a7495d6a4fc
eleanorsteinbrecher1 <els239@miami.edu>
2025-01-16 12:53:48 -0500

    hello world

--- dynamic data spring 2025 /hello-world/hello-world.js
+++ dynamic data spring 2025 /hello-world/hello-world.js
@@ -4,12 +4,11 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World;');  // Changed here
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/ ...Press ctrl + c to close`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
