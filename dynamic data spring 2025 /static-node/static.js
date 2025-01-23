f6857420678a9c46b21a8f39d075d76673215305
eleanorsteinbrecher1 <els239@miami.edu>
2025-01-23 13:55:13 -0500

    lab 2

--- dynamic data spring 2025 /static-node/static.js
+++ dynamic data spring 2025 /static-node/static.js
@@ -1,32 +1,35 @@
-
+// Import the HTTP and FS modules
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 4000;
 
-// funcions
-//syntax of a function
 const someName = (name) => {
     console.log("Your Name is " + name);
 };
 
-// Call the sample function
 someName("Elli Steinbrecher");
 
-
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.statusCode = 200; 
-    res.setHeader('Content-Type', 'text/plain'); 
-    fs.readFile ('/public/home.html'(error,content)=>{
-        res.end(content); 
-        //need to handle erros first
-        //if theres no errors,we can output the content
-    })
-    
+    if (req.url === '/' || req.url === '/home') {
+        fs.readFile('./public/home.html', (error, content) => {
+            if (error) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Error: Unable to load home.html');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(content);
+            }
+        });
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Error: Page not found');
+    }
 });
-
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
