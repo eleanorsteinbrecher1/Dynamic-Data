b3e3441e0a1b701785f8bcac9eef4a360720cd84
eleanorsteinbrecher1 <els239@miami.edu>
2025-01-28 17:06:35 -0500

    LAB 2
    LAB 2

--- dynamic data spring 2025 /static-node/static.js
+++ dynamic data spring 2025 /static-node/static.js
@@ -1,36 +1,57 @@
-// Import the HTTP and FS modules
+// Import necessary modules
 const http = require('http');
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
-const port = 4000;
+const port = 3000;
 
+// Functions
+// Syntax of a function
 const someName = (name) => {
-    console.log("Your Name is " + name);
+    console.log("Your name is " + name);
+};
+someName("Elli S");
+
+// Function to read and display files
+const displayPage = (path, res, status = 200) => {
+    res.statusCode = status;
+    res.setHeader('Content-Type', 'text/html');
+
+    fs.readFile(path, (error, content) => {
+        if (error) {
+            res.statusCode = 500;
+            res.end("<h1>500 Internal Server Error</h1>");
+        } else {
+            res.end(content);
+        }
+    });
 };
 
-someName("Elli Steinbrecher");
-
+// Create the server
 const server = http.createServer((req, res) => {
-    if (req.url === '/' || req.url === '/home') {
-        fs.readFile('./public/home.html', (error, content) => {
-            if (error) {
-                res.statusCode = 500;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end('Error: Unable to load home.html');
-            } else {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                res.end(content);
-            }
-        });
-    } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Error: Page not found');
+    console.log(`Request received: ${req.url}`);
+
+    switch (req.url) {
+        case "/":
+            // Serve home page
+            displayPage('./public/home.html', res);
+            break;
+        case "/about":
+            // Serve about page
+            displayPage('./public/about.html', res);
+            break;
+        case "/contact":
+            // Serve contact page
+            displayPage('./public/contact.html', res);
+            break;
+        default:
+            // Serve 404 error page (Optional)
+            displayPage('./public/404.html', res, 404);
+            break;
     }
 });
 
+// Start the server
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/ ...Press ctrl + c to close`);
 });
