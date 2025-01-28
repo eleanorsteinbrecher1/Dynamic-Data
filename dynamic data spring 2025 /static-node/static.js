0cc39d303b45f7b71db6ab1e8b2603efada6f075
eleanorsteinbrecher1 <els239@miami.edu>
2025-01-28 17:08:19 -0500

    LAB 2 final

--- dynamic data spring 2025 /static-node/static.js
+++ dynamic data spring 2025 /static-node/static.js
@@ -1,57 +1,65 @@
-// Import necessary modules
+//variables
 const http = require('http');
-const fs = require('fs');
+
+const fs = require('fs')
 
 const hostname = '127.0.0.1';
 const port = 3000;
-
 // Functions
-// Syntax of a function
-const someName = (name) => {
-    console.log("Your name is " + name);
-};
-someName("Elli S");
+// syntax of a function 
+const someName = (name)=>{
+    console.log("Your name is " + name)
+}
 
-// Function to read and display files
-const displayPage = (path, res, status = 200) => {
-    res.statusCode = status;
-    res.setHeader('Content-Type', 'text/html');
+someName("Elli S")
+// Create a function to read files and display them
+const displayPage = (path, r, status = 200)=> {
+  r.setHeader('Content-Type', 'text/html')
+  fs.readFile( path, (error,content)=>{
+      //We need to handle errors first
+      if(error) {
+        r.statusCode = 500
+        r.end("500 - error")
+      } else {
+      r.statusCode = status
+      //if there are no errors, we can output the content
+      r.end(content)
+      }
+  })
+}
 
-    fs.readFile(path, (error, content) => {
-        if (error) {
-            res.statusCode = 500;
-            res.end("<h1>500 Internal Server Error</h1>");
-        } else {
-            res.end(content);
-        }
-    });
-};
 
-// Create the server
 const server = http.createServer((req, res) => {
-    console.log(`Request received: ${req.url}`);
 
-    switch (req.url) {
-        case "/":
-            // Serve home page
-            displayPage('./public/home.html', res);
-            break;
-        case "/about":
-            // Serve about page
-            displayPage('./public/about.html', res);
-            break;
-        case "/contact":
-            // Serve contact page
-            displayPage('./public/contact.html', res);
-            break;
-        default:
-            // Serve 404 error page (Optional)
-            displayPage('./public/404.html', res, 404);
-            break;
-    }
+  console.log(req.url)
+  
+  switch (req.url) {
+    case "":  
+    case "/":
+    //Respond to /
+    displayPage('./public/home.html',res)
+    break;
+    
+    case "/about":
+    //Respond to /
+    displayPage('./public/about.html',res)
+    break;
+
+    case "/contact":
+    //Respond to /
+    displayPage('./public/contact.html',res)
+    break;
+
+    default:
+      //Respond to /
+      displayPage('./public/404.html',res,404)
+
+  }
+
+ 
+
 });
 
-// Start the server
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/ ...Press ctrl + c to close`);
+  console.log(`Server running at http://${hostname}:${port}/ ...Press ctrl + c to close`);
 });
